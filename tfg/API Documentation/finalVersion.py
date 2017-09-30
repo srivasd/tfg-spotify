95c70a00c80f34f970e218d476301f03edb5d1f7
srivasd <s.rivasd@alumnos.urjc.es>
2017-09-30 15:52:13 +0200

    Final version API part finished

---
+++ tfg/API Documentation/finalVersion.py
@@ -0,0 +1,125 @@
+import spotipy
+import spotipy.util as util
+import json
+import sys
+
+scope = 'user-library-read'
+
+username = 'srivasdelgado'
+
+token = util.prompt_for_user_token(username, scope)
+print(token)
+
+
+if token:
+    # Song (On top of the world)
+    main_features = []
+    actual_features = []
+    song_id = '4eLSCSELtKxZwXnFbNLXT5'
+    sp = spotipy.Spotify(auth=token)
+    offset = 0
+    limit = 2
+    song_info = sp.track(song_id)
+    # print(json.dumps(song_info, indent=1))
+    song_features = sp.audio_features(song_info['id'])
+    # print(json.dumps(track_info, indent=1))
+    print(song_info['name'])
+    for feature in song_features:
+        print('\tDanceability:', feature['danceability'])
+        main_features.append(feature['danceability'])
+        print('\tEnergy:', feature['energy'])
+        main_features.append(feature['energy'])
+        print('\tLiveness:', feature['liveness'])
+        main_features.append(feature['liveness'])
+        print('\tMode:', feature['mode'])
+        main_features.append(feature['mode'])
+        print('\tSpeechiness:', feature['speechiness'])
+        main_features.append(feature['speechiness'])
+        print('\tAcousticness:', feature['acousticness'])
+        main_features.append(feature['acousticness'])
+        print('\tInstrumentalness:', feature['instrumentalness'])
+        main_features.append(feature['instrumentalness'])
+        print('\tValence:', feature['valence'])
+        main_features.append(feature['valence'])
+    # Song's artist
+    # artistName = song_info['album']['artists'][0]['name']
+    artistId = song_info['album']['artists'][0]['id']
+    print(artistId)
+
+    # Related Artist's
+    sp = spotipy.Spotify(auth=token)
+    related_artists = sp.artist_related_artists(artistId)
+    # print(json.dumps(related_artists, indent=1))
+    for related_artist in related_artists['artists']:
+        print(related_artist['id'])
+
+        # Artist's related songs to the first one
+        # Artist's albums
+
+        sp = spotipy.Spotify(auth=token)
+        count = sys.maxsize
+        offset = 0
+        limit = 2
+        artist_albums_ids = []
+        while True:
+            artist_albums = sp.artist_albums(related_artist['id'], album_type='album', offset=offset, limit=limit)
+            offset += len(artist_albums['items'])
+            # print(json.dumps(artist_albums, indent=1))
+            # for album in artist_albums['items']:
+            for album in artist_albums['items']:
+                # print(album['id'])
+                artist_albums_ids.append(album['id'])
+                # Artist's songs
+                for artist_albums_id in artist_albums_ids:
+                    sp = spotipy.Spotify(auth=token)
+                    count = sys.maxsize
+                    offset = 0
+                    limit = 3
+                    while True:
+                        album_songs = sp.album_tracks(artist_albums_id, offset=offset, limit=limit)
+                        offset += len(album_songs['items'])
+                        # print(json.dumps(album_info, indent=1))
+                        cont = 1
+                        for song in album_songs['items']:
+                            track_info = sp.audio_features(song['id'])
+                            # print(json.dumps(track_info, indent=1))
+                            for feature in track_info:
+                                change_feature = 0
+                                actual_features.append(feature['danceability'])
+                                actual_features.append(feature['energy'])
+                                actual_features.append(feature['liveness'])
+                                actual_features.append(feature['mode'])
+                                actual_features.append(feature['speechiness'])
+                                actual_features.append(feature['acousticness'])
+                                actual_features.append(feature['instrumentalness'])
+                                actual_features.append(feature['valence'])
+                                i = 0
+                                while i <= 7:
+                                    change_feature = abs(actual_features[i] - main_features[i]) + change_feature
+                                    i = i + 1
+
+                                if change_feature < 1:
+                                    print('Song', cont, ':', song['name'])
+
+                                    print('\tDanceability:', feature['danceability'])
+                                    print('\tEnergy:', feature['energy'])
+                                    print('\tLiveness:', feature['liveness'])
+                                    print('\tMode:', feature['mode'])
+                                    print('\tSpeechiness:', feature['speechiness'])
+                                    print('\tAcousticness:', feature['acousticness'])
+                                    print('\tInstrumentalness:', feature['instrumentalness'])
+                                    print('\tValence:', feature['valence'])
+                            cont += 1
+                        # print(json.dumps(album_info, indent=1))
+                        if len(album_songs['items']) < limit:
+                            break
+                    break
+            if len(artist_albums['items']) < limit:
+                break
+
+
+# Repeat the process
+
+
+else:
+    print("Can't get token for", username)
