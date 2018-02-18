e7740f206c773c2b58faba3acfdb2947d6fd0149
srivasd <s.rivasd@alumnos.urjc.es>
2018-02-18 16:10:01 +0100

    Input value changed

--- tfg/Flask/finalversion-songs/finalversion-songs.py
+++ tfg/Flask/finalversion-songs/finalversion-songs.py
@@ -36,155 +36,156 @@ def close_db(error):
 def get_index():
     db = get_db()
 
-    song_proof = request.args.get('song', default = 'On Top Of The World', type = str)
+    song_proof = request.args.get('song', default = '', type = str)
     print("Cancion parametro: " + song_proof)
 
-    if token:
-        # Song (On top of the world)
-        main_features = []
-        actual_features = []
-        songs_checked = []
-        related_artists_checked = []
-        related_artist_global = ""
-        related_song_global = ""
-        main_song = ""
-        main_artist = ""
-        sp = spotipy.Spotify(auth=token)
-
-        result = sp.search(song_proof, type='track')
-        song_id = result['tracks']['items'][0]['id']
-
-        offset = 0
-        limit = 2
-        song_info = sp.track(song_id)
-        # print(json.dumps(song_info, indent=1))
-        db.run("CREATE (s:Song {name: {name}, main: {main}})",
-               {"name": song_info['name'], "main": True})
-        main_song = song_info['name']
-        song_features = sp.audio_features(song_info['id'])
-        # print(json.dumps(track_info, indent=1))
-        print(song_info['name'])
-        for feature in song_features:
-            print('\tDanceability:', feature['danceability'])
-            main_features.append(feature['danceability'])
-            print('\tEnergy:', feature['energy'])
-            main_features.append(feature['energy'])
-            print('\tLiveness:', feature['liveness'])
-            main_features.append(feature['liveness'])
-            print('\tMode:', feature['mode'])
-            main_features.append(feature['mode'])
-            print('\tSpeechiness:', feature['speechiness'])
-            main_features.append(feature['speechiness'])
-            print('\tAcousticness:', feature['acousticness'])
-            main_features.append(feature['acousticness'])
-            print('\tInstrumentalness:', feature['instrumentalness'])
-            main_features.append(feature['instrumentalness'])
-            print('\tValence:', feature['valence'])
-            main_features.append(feature['valence'])
-        # Song's artist
-        main_artist = song_info['album']['artists'][0]['name']
-        artistId = song_info['album']['artists'][0]['id']
-        print(artistId)
-
-        db.run("CREATE (ar:Artist {name: {name}, main: {main}})",
-                    {"name": song_info['album']['artists'][0]['name'], "main": True})
-
-        db.run("MATCH (s:Song),(ar:Artist) CREATE (s)-[r: ARTIST]->(ar) RETURN r")
-        # Related Artist's
-        sp = spotipy.Spotify(auth=token)
-        related_artists = sp.artist_related_artists(artistId)
-        # print(json.dumps(related_artists, indent=1))
-        for related_artist in related_artists['artists']:
-            print(related_artist['id'])
-            # Artist's related songs to the first one
-            # Artist's albums
-
+    if song_proof != '':
+        if token:
+            # Song (On top of the world)
+            main_features = []
+            actual_features = []
+            songs_checked = []
+            related_artists_checked = []
+            related_artist_global = ""
+            related_song_global = ""
+            main_song = ""
+            main_artist = ""
             sp = spotipy.Spotify(auth=token)
-            count = sys.maxsize
+
+            result = sp.search(song_proof, type='track')
+            song_id = result['tracks']['items'][0]['id']
+
             offset = 0
             limit = 2
-            artist_albums_ids = []
-            while True:
-                artist_albums = sp.artist_albums(related_artist['id'], album_type='album', offset=offset, limit=limit)
-                offset += len(artist_albums['items'])
-                # print(json.dumps(artist_albums, indent=1))
-                # for album in artist_albums['items']:
-                for album in artist_albums['items']:
-                    # print(album['id'])
-                    artist_albums_ids.append(album['id'])
-                    # Artist's songs
-                    for artist_albums_id in artist_albums_ids:
-                        sp = spotipy.Spotify(auth=token)
-                        count = sys.maxsize
-                        offset = 0
-                        limit = 3
-                        while True:
-                            album_songs = sp.album_tracks(artist_albums_id, offset=offset, limit=limit)
-                            offset += len(album_songs['items'])
-                            # print(json.dumps(album_info, indent=1))
-                            cont = 1
-                            for song in album_songs['items']:
-                                track_info = sp.audio_features(song['id'])
-                                # print(json.dumps(track_info, indent=1))
-                                for feature in track_info:
-                                    change_feature = 0
-                                    actual_features.append(feature['danceability'])
-                                    actual_features.append(feature['energy'])
-                                    actual_features.append(feature['liveness'])
-                                    actual_features.append(feature['mode'])
-                                    actual_features.append(feature['speechiness'])
-                                    actual_features.append(feature['acousticness'])
-                                    actual_features.append(feature['instrumentalness'])
-                                    actual_features.append(feature['valence'])
-                                    i = 0
-                                    while i <= 7:
-                                        if isinstance(actual_features[i], float):
-                                            change_feature = abs(actual_features[i] - main_features[i]) + change_feature
-                                        i = i + 1
-                                    actual_features.clear()
-                                    if change_feature < 0.5 and song not in songs_checked:
-                                        print('Song', cont, ':', song['name'])
+            song_info = sp.track(song_id)
+            # print(json.dumps(song_info, indent=1))
+            db.run("CREATE (s:Song {name: {name}, main: {main}})",
+                   {"name": song_info['name'], "main": True})
+            main_song = song_info['name']
+            song_features = sp.audio_features(song_info['id'])
+            # print(json.dumps(track_info, indent=1))
+            print(song_info['name'])
+            for feature in song_features:
+                print('\tDanceability:', feature['danceability'])
+                main_features.append(feature['danceability'])
+                print('\tEnergy:', feature['energy'])
+                main_features.append(feature['energy'])
+                print('\tLiveness:', feature['liveness'])
+                main_features.append(feature['liveness'])
+                print('\tMode:', feature['mode'])
+                main_features.append(feature['mode'])
+                print('\tSpeechiness:', feature['speechiness'])
+                main_features.append(feature['speechiness'])
+                print('\tAcousticness:', feature['acousticness'])
+                main_features.append(feature['acousticness'])
+                print('\tInstrumentalness:', feature['instrumentalness'])
+                main_features.append(feature['instrumentalness'])
+                print('\tValence:', feature['valence'])
+                main_features.append(feature['valence'])
+            # Song's artist
+            main_artist = song_info['album']['artists'][0]['name']
+            artistId = song_info['album']['artists'][0]['id']
+            print(artistId)
 
-                                        print('\tDanceability:', feature['danceability'])
-                                        print('\tEnergy:', feature['energy'])
-                                        print('\tLiveness:', feature['liveness'])
-                                        print('\tMode:', feature['mode'])
-                                        print('\tSpeechiness:', feature['speechiness'])
-                                        print('\tAcousticness:', feature['acousticness'])
-                                        print('\tInstrumentalness:', feature['instrumentalness'])
-                                        print('\tValence:', feature['valence'])
-                                        songs_checked.append(song)
+            db.run("CREATE (ar:Artist {name: {name}, main: {main}})",
+                        {"name": song_info['album']['artists'][0]['name'], "main": True})
 
-                                        if related_artist not in related_artists_checked:
-                                            related_artist_global = related_artist['name']
-                                            cypher_artist = "CREATE (ar:Artist {name: {name}, main: {main}})"
-                                            db.run(cypher_artist,
-                                                        {"name": related_artist['name'], "main": False})
+            db.run("MATCH (s:Song),(ar:Artist) CREATE (s)-[r: ARTIST]->(ar) RETURN r")
+            # Related Artist's
+            sp = spotipy.Spotify(auth=token)
+            related_artists = sp.artist_related_artists(artistId)
+            # print(json.dumps(related_artists, indent=1))
+            for related_artist in related_artists['artists']:
+                print(related_artist['id'])
+                # Artist's related songs to the first one
+                # Artist's albums
 
-                                            db.run("CREATE (s:Song {name: {name}, artist: {artist}, main: {main}})",
-                                                    {"name": song['name'], "artist": song['artists'][0]['name'], "main": False})
-                                        related_song_global = song['name']
-                                        db.run(
-                                            "MATCH (ar:Artist),(s:Song) WHERE ar.name = \"" + related_artist_global + "\" AND s.name = \"" + related_song_global + "\" CREATE (ar)-[r:RELATED_SONG]->(s) RETURN r")
-                                        related_artists_checked.append(related_artist)
-                                cont += 1
-                            # print(json.dumps(album_info, indent=1))
-                            if len(album_songs['items']) < limit:
-                                break
+                sp = spotipy.Spotify(auth=token)
+                count = sys.maxsize
+                offset = 0
+                limit = 2
+                artist_albums_ids = []
+                while True:
+                    artist_albums = sp.artist_albums(related_artist['id'], album_type='album', offset=offset, limit=limit)
+                    offset += len(artist_albums['items'])
+                    # print(json.dumps(artist_albums, indent=1))
+                    # for album in artist_albums['items']:
+                    for album in artist_albums['items']:
+                        # print(album['id'])
+                        artist_albums_ids.append(album['id'])
+                        # Artist's songs
+                        for artist_albums_id in artist_albums_ids:
+                            sp = spotipy.Spotify(auth=token)
+                            count = sys.maxsize
+                            offset = 0
+                            limit = 3
+                            while True:
+                                album_songs = sp.album_tracks(artist_albums_id, offset=offset, limit=limit)
+                                offset += len(album_songs['items'])
+                                # print(json.dumps(album_info, indent=1))
+                                cont = 1
+                                for song in album_songs['items']:
+                                    track_info = sp.audio_features(song['id'])
+                                    # print(json.dumps(track_info, indent=1))
+                                    for feature in track_info:
+                                        change_feature = 0
+                                        actual_features.append(feature['danceability'])
+                                        actual_features.append(feature['energy'])
+                                        actual_features.append(feature['liveness'])
+                                        actual_features.append(feature['mode'])
+                                        actual_features.append(feature['speechiness'])
+                                        actual_features.append(feature['acousticness'])
+                                        actual_features.append(feature['instrumentalness'])
+                                        actual_features.append(feature['valence'])
+                                        i = 0
+                                        while i <= 7:
+                                            if isinstance(actual_features[i], float):
+                                                change_feature = abs(actual_features[i] - main_features[i]) + change_feature
+                                            i = i + 1
+                                        actual_features.clear()
+                                        if change_feature < 0.5 and song not in songs_checked:
+                                            print('Song', cont, ':', song['name'])
+
+                                            print('\tDanceability:', feature['danceability'])
+                                            print('\tEnergy:', feature['energy'])
+                                            print('\tLiveness:', feature['liveness'])
+                                            print('\tMode:', feature['mode'])
+                                            print('\tSpeechiness:', feature['speechiness'])
+                                            print('\tAcousticness:', feature['acousticness'])
+                                            print('\tInstrumentalness:', feature['instrumentalness'])
+                                            print('\tValence:', feature['valence'])
+                                            songs_checked.append(song)
+
+                                            if related_artist not in related_artists_checked:
+                                                related_artist_global = related_artist['name']
+                                                cypher_artist = "CREATE (ar:Artist {name: {name}, main: {main}})"
+                                                db.run(cypher_artist,
+                                                            {"name": related_artist['name'], "main": False})
+
+                                                db.run("CREATE (s:Song {name: {name}, artist: {artist}, main: {main}})",
+                                                        {"name": song['name'], "artist": song['artists'][0]['name'], "main": False})
+                                            related_song_global = song['name']
+                                            db.run(
+                                                "MATCH (ar:Artist),(s:Song) WHERE ar.name = \"" + related_artist_global + "\" AND s.name = \"" + related_song_global + "\" CREATE (ar)-[r:RELATED_SONG]->(s) RETURN r")
+                                            related_artists_checked.append(related_artist)
+                                    cont += 1
+                                # print(json.dumps(album_info, indent=1))
+                                if len(album_songs['items']) < limit:
+                                    break
+                            break
+                    if len(artist_albums['items']) < limit:
                         break
-                if len(artist_albums['items']) < limit:
-                    break
 
-        db.run(
-            "MATCH (ar:Artist),(ar2:Artist) WHERE ar.name = \"" + main_artist + "\" AND NOT ar2.name = \"" + main_artist + "\"CREATE (ar)-[r: RELATED_ARTIST]->("
-                                                                                                                           "ar2) RETURN r")
+            db.run(
+                "MATCH (ar:Artist),(ar2:Artist) WHERE ar.name = \"" + main_artist + "\" AND NOT ar2.name = \"" + main_artist + "\"CREATE (ar)-[r: RELATED_ARTIST]->("
+                                                                                                                               "ar2) RETURN r")
 
 
-    # Repeat the process
+        # Repeat the process
 
 
-    else:
-        print("Can't get token for", username)
+        else:
+            print("Can't get token for", username)
 
     return app.send_static_file('index.html')
 
